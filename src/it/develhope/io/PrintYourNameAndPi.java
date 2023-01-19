7c2dea8551aeeff59ae59b9de8e287a4a87dfa2c
Alessio Limina <alessio.limina@gmail.com>
2023-01-19 22:11:40 +0100

    Exercise Input and Putput 04

---
+++ src/it/develhope/io/PrintYourNameAndPi.java
@@ -0,0 +1,12 @@
+package it.develhope.io;
+
+public class PrintYourNameAndPi {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%S %n", "Alessio");
+        System.out.printf("The number Pi is %f ", 3.141590);
+
+    }
+
+}
